88826ff48f42bde4cd557bbd1738fd6d28f0a537
Bernd Eckenfels <b.eckenfels@seeburger.de>
2013-09-06 23:51:45 +0200

    Cleanup unused code and comments.

--- vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/provider/jdbctable/JdbcTableProvider.java
+++ vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/provider/jdbctable/JdbcTableProvider.java
@@ -1,5 +1,6 @@
 package com.seeburger.vfs2.provider.jdbctable;
 
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -12,19 +13,12 @@ import org.apache.commons.vfs2.FileSystem;
 import org.apache.commons.vfs2.FileSystemConfigBuilder;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemOptions;
-import org.apache.commons.vfs2.UserAuthenticationData;
 import org.apache.commons.vfs2.provider.AbstractOriginatingFileProvider;
-import org.apache.commons.vfs2.util.UserAuthenticatorUtils;
+
 
 public class JdbcTableProvider
     extends AbstractOriginatingFileProvider
 {
-    /** Authenticator information. */
-    public static final UserAuthenticationData.Type[] AUTHENTICATOR_TYPES = new UserAuthenticationData.Type[]
-        {
-            UserAuthenticationData.USERNAME, UserAuthenticationData.PASSWORD
-        };
-
     static final Collection<Capability> capabilities = Collections.unmodifiableCollection(Arrays.asList(new Capability[]
     {
         Capability.CREATE,
@@ -46,16 +40,9 @@ public class JdbcTableProvider
 
 	DataSource dataSource;
 
-    /**
-     * Constructs a new provider.
-     */
-    public JdbcTableProvider()
-    {
-    	this(null);
-    }
 
-    /**
-     * Constructs a new provider.
+	/**
+     * Constructs a new provider for given DataSource.
      */
     public JdbcTableProvider(DataSource dataSource)
     {
@@ -64,27 +51,12 @@ public class JdbcTableProvider
         this.dataSource = dataSource;
     }
 
-
-    /**
-     * Creates a {@link FileSystem}.
-     */
     @Override
     protected FileSystem doCreateFileSystem(final FileName name, final FileSystemOptions fileSystemOptions)
         throws FileSystemException
     {
-    	// name can be instance of GenericFileName or URLFileName
+        // name can be instance of GenericFileName or URLFileName
         //final URLFileName rootName = (UrlFileNameParser) name;
-        UserAuthenticationData authData = null;
-        try
-        {
-            authData = UserAuthenticatorUtils.authenticate(fileSystemOptions, AUTHENTICATOR_TYPES);
-            System.out.println("auth=" + ((authData == null)?"(null)":(authData.toString() + authData.getData(UserAuthenticationData.USERNAME))));
-        }
-        finally
-        {
-            UserAuthenticatorUtils.cleanup(authData);
-        }
-
         return new JdbcTableFileSystem(name, this, fileSystemOptions);
     }
 
