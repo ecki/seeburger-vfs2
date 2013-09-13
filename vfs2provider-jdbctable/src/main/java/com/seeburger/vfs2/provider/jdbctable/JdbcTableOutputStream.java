40946aca4992e6861a45511a4628adc26335980a
Bernd Eckenfels <b.eckenfels@seeburger.de>
2013-09-13 23:34:08 +0200

    removed import warning.

--- vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/provider/jdbctable/JdbcTableOutputStream.java
+++ vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/provider/jdbctable/JdbcTableOutputStream.java
@@ -5,8 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import org.apache.commons.vfs2.provider.AbstractFileObject;
-
 
 public class JdbcTableOutputStream extends OutputStream
 {
