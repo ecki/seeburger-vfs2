dc8e523c3bc8a79ad2776f7ce4b3938e03964741
Bernd Eckenfels <b.eckenfels@seeburger.de>
2013-10-02 17:57:11 +0200

    added utility function to hide casting/fsoperations access to commit.

--- vfs2provider-digestarc/src/main/java/com/seeburger/vfs2/provider/digestarc/DarcFileUtil.java
+++ vfs2provider-digestarc/src/main/java/com/seeburger/vfs2/provider/digestarc/DarcFileUtil.java
@@ -8,7 +8,10 @@
 package com.seeburger.vfs2.provider.digestarc;
 
 
+import java.io.IOException;
+
 import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystem;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.impl.DecoratedFileObject;
 
@@ -69,5 +72,32 @@ public class DarcFileUtil
         }
         // TODO: fallback
         return null;
+    }
+
+
+    /**
+     * Commit the changes to the filesystem owning the specified file and return new treeID.
+     *
+     * @param file any file inside a DarcFileSystem
+     * @return the new commitID or null if no changes
+     * @throws IOException if the commit operation failed the exception is forwarded
+     * @throws IllegalArgumentException if the specified file cannot be casted to a DarcFileSystem
+     */
+    public static String commitChanges(FileObject file) throws IOException
+    {
+        DarcFileObject darcFile = unwrapDarcFile(file);
+        if (darcFile == null)
+        {
+            throw new IllegalArgumentException("For commiting the digest archive you need to specify a DarcFileObject: " + file);
+        }
+
+        FileSystem fs = darcFile.getFileSystem();
+        if (!(fs instanceof DarcFileSystem))
+        {
+            throw new IllegalArgumentException("The class=" + fs.getClass() + " is no DarcFileSystem for " + file);
+        }
+
+        DarcFileSystem dfs = (DarcFileSystem)fs;
+        return dfs.commitChanges();
     }
 }
