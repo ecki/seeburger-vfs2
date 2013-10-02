8376a2041b3ed25ae3166204cc56d141297ff313
Bernd Eckenfels <b.eckenfels@seeburger.de>
2013-10-02 02:02:58 +0200

    sql cleanup

--- vfs2provider-jdbctable/src/test/resources/db/migration/h2_derby/V20130909__Initial_Table.sql
+++ vfs2provider-jdbctable/src/test/resources/db/migration/h2_derby/V20130909__Initial_Table.sql
@@ -1,19 +1,19 @@
 CREATE TABLE tBlobs (
-    cParent VARCHAR(255) NOT NULL,
-    cName VARCHAR(127) NOT NULL,
-    cSize BIGINT NOT NULL,
+    cParent       VARCHAR(255) NOT NULL,
+    cName         VARCHAR(127) NOT NULL,
+    cSize         BIGINT NOT NULL,
     cLastModified BIGINT NOT NULL,
-    cMarkGarbage BIGINT,
-    cBlob BLOB,
+    cMarkGarbage  BIGINT,
+    cBlob         BLOB,
   CONSTRAINT PK_Blobs_name_parent PRIMARY KEY(cParent, cName)
 );
 
 CREATE TABLE tBlobs2 (
-    cParent VARCHAR(255) NOT NULL,
-    cName VARCHAR(127) NOT NULL,
-    cSize BIGINT NOT NULL,
+    cParent       VARCHAR(255) NOT NULL,
+    cName         VARCHAR(127) NOT NULL,
+    cSize         BIGINT NOT NULL,
     cLastModified BIGINT NOT NULL,
-    cMarkGarbage BIGINT,
-    cBlob BLOB,
+    cMarkGarbage  BIGINT,
+    cBlob         BLOB,
   CONSTRAINT PK_Blobs2_name_parent PRIMARY KEY(cParent, cName)
 );
