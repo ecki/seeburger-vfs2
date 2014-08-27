a783c8d19056c5378f923b2e551ec92636b5eba9
Bernd Eckenfels <b.eckenfels@seeburger.de>
2014-08-27 17:17:14 +0200

    Skip printing of date if it was not set. Improved javadoc.

--- vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/util/TreePrinter.java
+++ vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/util/TreePrinter.java
@@ -23,7 +23,21 @@ import org.apache.commons.vfs2.FileType;
 public class TreePrinter
 {
     /**
-     * Print the content of file and its childs to the given out writer.
+     * Print the content of file and its childs to the given print stream.
+     * <P>
+     * Format is <code>FILENAME (dHRW)[[ date=DATE] size=SIZE att=ATTRIB]</code>
+     * <P>
+     * d=Directory (has child); H=Hidden; R=is Readable;W=is writeable. Unset flags are represented as {@code .}.
+     * <p>
+     * DATE is last modified date (skipped if 0).
+     * <p>
+     * This implementation is recursive, it is not intended for production uses. Use it only for
+     * diagnostic and debugging utilities.
+     *
+     * @param file the file node to recursively print
+     * @param prefix text prefix to print at beginning of lines (will be extended with blank for deeper levels).
+     * @param out target to write the formatted lines to
+     * @throws FileSystemException passed unmodified from VFS methods.
      */
     public static void printTree(FileObject file, String prefix, PrintStream out) throws FileSystemException
     {
@@ -44,9 +58,28 @@ public class TreePrinter
         FileContent content = file.getContent();
         if (content != null)
         {
-            try { type += " date=" + new Date(content.getLastModifiedTime()); }catch (Exception ig) { }
-            try { type += " size=" + content.getSize();  }catch (Exception ig) { }
-            try { type += " att=" + content.getAttributes();  }catch (Exception ig) { }
+            // print only file dates which are not 0 (1970-01-01)
+            try
+            {
+                long last = content.getLastModifiedTime();
+                if (last != 0)
+                {
+                    type += " date=" + new Date(last);
+                }
+            }
+            catch (Exception ig) { /* ignored */ }
+
+            try
+            {
+                type += " size=" + content.getSize();
+            }
+            catch (Exception ig) { /* ignored */ }
+
+            try
+            {
+                type += " att=" + content.getAttributes();
+            }
+            catch (Exception ig) { /* ignored */  }
         }
         String fileName = file.getName().getPathDecoded();
         if (prefix.length() + fileName.length() < 60)
