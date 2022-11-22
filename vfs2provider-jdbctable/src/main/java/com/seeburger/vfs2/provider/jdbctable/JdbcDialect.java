7fa0c2d5092b151d99003d3655c03dcc2bc0fe71
Bernd Eckenfels <b.eckenfels@seeburger.de>
2022-11-22 11:56:09 +0100

    Fix missing javadoc parameter.

--- vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/provider/jdbctable/JdbcDialect.java
+++ vfs2provider-jdbctable/src/main/java/com/seeburger/vfs2/provider/jdbctable/JdbcDialect.java
@@ -48,7 +48,7 @@ public interface JdbcDialect
      * It is expected auto-commit is turned off for this connection and it
      * is actually lend from a pool.
      *
-     * @param diagnostics string for logging the caller operation
+     * @param where diagnostics string for logging the caller operation
      * @throws SQLException
      */
     Connection getConnection(String where) throws SQLException;
